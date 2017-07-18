f8f3f0286834d34d314ab078a63197836931ddda
juan <juan@juan-MS-7817>
2017-07-18 15:57:25 -0500

    mi primero hola

--- src/AppBundle/Controller/CitasController.php
+++ src/AppBundle/Controller/CitasController.php
@@ -1,17 +1,19 @@
 <?php
 
 namespace AppBundle\Controller;
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 
-/**
- * Description of CitasController
- *
- * @author juan
- */
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Symfony\Component\HttpFoundation\Response;
+
 class CitasController {
-    //put your code here
+
+    /**
+     * @Route("/citas")
+     */
+    public function ShowAction() 
+    {
+     
+        return new Response("hola");
+    }
+
 }
