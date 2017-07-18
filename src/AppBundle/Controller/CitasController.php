fc338ff25b5e0590ed61ca2df03c6efaa1eea8b5
juan <juan@juan-MS-7817>
2017-07-18 15:15:04 -0500

    modificacion composer.json

---
+++ src/AppBundle/Controller/CitasController.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace AppBundle\Controller;
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ * Description of CitasController
+ *
+ * @author juan
+ */
+class CitasController {
+    //put your code here
+}
