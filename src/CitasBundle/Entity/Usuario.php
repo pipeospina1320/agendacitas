88a8f93be43268f8b32ededeef4d8585caa5b5bf
pipe <pipe@Felipe>
2017-07-28 21:57:04 -0500

    Usuario

---
+++ src/CitasBundle/Entity/Usuario.php
@@ -0,0 +1,62 @@
+<?php
+
+namespace CitasBundle\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * Usuario
+ *
+ * @ORM\Table(name="usuario")
+ * @ORM\Entity(repositoryClass="CitasBundle\Repository\UsuarioRepository")
+ */
+class Usuario {
+
+    /**
+     * @var int
+     *
+     * @ORM\Column(name="codigo_usuario_pk", type="integer" , length=10)
+     * @ORM\Id
+     * @ORM\GeneratedValue(strategy="AUTO")
+     */
+    private $codigoUsuarioPk;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="nombre", type="string", length=255)
+     */
+    private $nombre;
+
+    /**
+     * Get codigoUsuarioPk
+     *
+     * @return integer
+     */
+    public function getCodigoUsuarioPk() {
+        return $this->codigoUsuarioPk;
+    }
+
+    /**
+     * Set nombre
+     *
+     * @param string $nombre
+     *
+     * @return Usuario
+     */
+    public function setNombre($nombre) {
+        $this->nombre = $nombre;
+
+        return $this;
+    }
+
+    /**
+     * Get nombre
+     *
+     * @return string
+     */
+    public function getNombre() {
+        return $this->nombre;
+    }
+
+}
