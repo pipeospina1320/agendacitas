9d23b97940f6d2c65bb43856f28f9f9699b1eb88
pipe <pipe@Felipe>
2017-07-29 22:11:27 -0500

    df

---
+++ src/CitasBundle/Form/UsuarioType.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace CitasBundle\Form;
+
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class UsuarioType extends AbstractType
+{
+    /**
+     * {@inheritdoc}
+     */
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder->add('nombre');
+    }
+    
+    /**
+     * {@inheritdoc}
+     */
+    public function configureOptions(OptionsResolver $resolver)
+    {
+        $resolver->setDefaults(array(
+            'data_class' => 'CitasBundle\Entity\Usuario'
+        ));
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function getBlockPrefix()
+    {
+        return 'citasbundle_usuario';
+    }
+
+
+}
